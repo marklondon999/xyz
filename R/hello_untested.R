23495ee1c133405e5576bd18628cffc17cacff00
London <Mark.London@ext.ons.gov.uk>
2025-01-16 11:21:11 +0000

    asdfs

---
+++ R/hello_untested.R
@@ -0,0 +1,16 @@
+#' @title hello_untested
+#' @description  A happy greeting made for RDSA, BUT UNTESTED
+#' @author Mark London \email{mark.london@ext.ons.gov.uk}
+#' @details # This is an example function named 'hello'
+#'  which prints 'Hello, world!'.
+#' @param NONE description
+#' @return None
+#' @examples
+#' hello_untested()
+#' @export
+hello_untested <- function() {
+  print("Hello, world!")
+}
+
+
+
--- setup.R
+++ setup.R
@@ -13,3 +13,28 @@ install.packages("D:/coding_repos/xyz_0.1.0.tar.gz", repos = NULL, type = "sourc
 
 
 ?hello()
+git remote add origin https://github.com/marklondon999/xyz
+
+git remote -v
+git add .
+# usethis::use_github_action("test-coverage")
+git branch -M main
+
+
+
+usethis::use_readme_rmd()
+
+detach("package:xyz", unload=TRUE)
+covr::package_coverage(path = ".",
+                       type = "all",
+                       combine_types = FALSE,
+                       relative_path = TRUE,
+                       quiet = TRUE,
+                       clean = TRUE,
+                       line_exclusions = NULL,
+                       function_exclusions = NULL,
+                       pre_clean = TRUE)
+
+
+library(xyz)
+rmarkdown::render("README.Rmd")
